dbd257dc2583dfa394ce971b5df9ba62e0bd968e
Odyseus <venusyadonis@gmail.com>
2018-12-15 07:30:22 -0300

    __init__.py
    - Homogenization of the __appdescription__ variable.

--- AppData/BackupUtilsApp/__init__.py
+++ AppData/BackupUtilsApp/__init__.py
@@ -3,5 +3,5 @@
 
 __status__ = "Stable"
 __appname__ = "Backup Utils"
-__appdescription__ = "A CLI utility written in Python 3 to backup files on GNU/Linux."
+__appdescription__ = "CLI utility to backup files on GNU/Linux."
 __version__ = "0.0.0"
