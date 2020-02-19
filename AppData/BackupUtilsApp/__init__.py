99b834e5ece534b5f5eb305e99cc35b8908bcab3
Odyseus <venusyadonis@gmail.com>
2020-02-19 12:01:41 -0300

    Updated manual page and bump version

--- AppData/BackupUtilsApp/__init__.py
+++ AppData/BackupUtilsApp/__init__.py
@@ -3,7 +3,7 @@
 __status__ = "Stable"
 __appname__ = "Backup Utils"
 __appdescription__ = "CLI utility to backup files on GNU/Linux."
-__version__ = "2020-02-14 03:47:21.873"
+__version__ = "2020-02-19 12:00:13.463"
 
 if __name__ == "__main__":
     pass
